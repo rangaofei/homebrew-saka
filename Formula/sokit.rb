473c058aa33ab0b1b2970de47dd889390f605973
saka <876038559@qq.com>
2018-02-04 22:19:10 +0800

    a

--- Formula/sokit.rb
+++ Formula/sokit.rb
@@ -1,31 +1,21 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook.html
 #                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
-class Sweather < Formula
-  desc "一个非常简单的获取天气客户端\n    ----by saka"
-  homepage "https://juejin.im/user/5855ebea8d6d810065a4befa/posts"
-  url "https://raw.githubusercontent.com/rangaofei/SWeather/master/pack/sweather-1.2.5.tar.gz"
-  sha256 "bb30dcef08f67e70d5af3ccb14d79012f65efaa8c0118b94f0a634dd3fb44eb8"
-
-  depends_on "cmake" => :build
-#  depends_on "curl"=>:run
-  def install
-    etc.install Dir["src/*"]
-    mkdir "build" do
-      system "cmake", "..",*std_cmake_args
-      system "make"
-      system "make", "install" # if this fails, try separate make/make install steps
-
-    end
-  end
-
-  test do
+ class Sokit < Formula
+  desc "The origin author and page:\nhttps://github.com/sinpolib/sokit\nfamous tool for debug with socket\n ----by saka"
+  homepage https://juejin.im/post/5a770f17f265da4e80778caa""
+  url "http://p3lublj5c.bkt.clouddn.com/sokit-1.1.0.tar.gz"
+  sha256 "0baf65c42a267f31e9fac83e67af784a312206126d9832e22f299e701ee7707c"
+   # depends_on "cmake" => :build
+   def install
+	   bin.install "sokit"
+   end
+   test do
     # `test do` will create, run in and delete a temporary directory.
     #
     # This test will fail and we won't accept that! For Homebrew/homebrew-core
     # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test sweather`. Options passed
+    # software. Run the test with `brew test sokit`. Options passed
     # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
     #
     # The installed folder is not in the path, so use the entire path to any
