35f115f26c930fc3e4c6e0fa7d040fb670c20433
saka <876038559@qq.com>
2018-04-17 11:53:55 +0800

    delete run

--- Formula/sokit.rb
+++ Formula/sokit.rb
@@ -7,7 +7,7 @@
    url "http://p3lublj5c.bkt.clouddn.com/sokit-1.1.1.tar.gz"
    sha256 "78913c71cd3afdc2ab5d43ed5dbe51abf7d2c7e78959e210766c280b78e82cb8"
    version "1.1.1"
-   depends_on "qt" => :run
+   depends_on "qt"
    def install
 	   bin.install "sokit"
    end
