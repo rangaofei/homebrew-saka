fe992c94202c5cc8ce73a0f59ed8bd4ae517f3e0
saka <876038559@qq.com>
2018-07-25 09:55:41 +0800

    new version:sweather-1\.3\.3

--- Formula/sweather.rb
+++ Formula/sweather.rb
@@ -6,7 +6,7 @@ class Sweather < Formula
   desc "一个非常简单的获取天气客户端\n    ----by saka"
   homepage "https://juejin.im/user/5855ebea8d6d810065a4befa/posts"
   url "https://raw.githubusercontent.com/rangaofei/SWeather/master/pack/sweather-1.3.1.tar.gz"
-  sha256 "2239538646e8bd50d555ad25f6c635db4e45cb80e050857c6a488731a7f2043f"
+  sha256 "6a8fce4a6c6e5f12abfdf6b974eadd8389754f0db9910441a40ad8ac2c772b90"
 
   depends_on "cmake" => :build
 #  depends_on "curl"=>:run
