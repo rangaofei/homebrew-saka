803851b2bc7026bdebe276b09a892916270991e3
rangaofei <876038559@qq.com>
2019-02-14 17:29:17 +0800

    Update sokit.rb

--- Formula/sokit.rb
+++ Formula/sokit.rb
@@ -1,6 +1,4 @@
-#Documentation: https://docs.brew.sh/Formula-Cookbook.html
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
  class Sokit < Formula
   desc "The origin author and page:\nhttps://github.com/sinpolib/sokit\nfamous tool for debug with socket\n ----by saka"
    homepage "https://juejin.im/post/5a770f17f265da4e80778caa"
@@ -12,15 +10,7 @@
 	   bin.install "sokit"
    end
    test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test sokit`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
+ 
     system "false"
   end
 end
