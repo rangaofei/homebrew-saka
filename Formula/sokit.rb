ba6022615fa701116f8633838bc447ead8b69499
rangaofei <876038559@qq.com>
2019-02-17 20:24:12 +0800

    Update sokit.rb

--- Formula/sokit.rb
+++ Formula/sokit.rb
@@ -2,10 +2,9 @@
  class Sokit < Formula
   desc "The origin author and page:\nhttps://github.com/sinpolib/sokit\nfamous tool for debug with socket\n ----by saka"
    homepage "https://juejin.im/post/5a770f17f265da4e80778caa"
-   url "http://img.rangaofei.cn/sokit-1.1.1.tar.gz"
+   url "https://raw.githubusercontent.com/rangaofei/SWeather/master/pack/sokit-1.1.1.tar.gz"
    sha256 "78913c71cd3afdc2ab5d43ed5dbe51abf7d2c7e78959e210766c280b78e82cb8"
    version "1.1.1"
-   depends_on "qt"
    def install
 	   bin.install "sokit"
    end
