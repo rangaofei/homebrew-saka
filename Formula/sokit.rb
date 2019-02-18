600bbbba058730ad8e6c7d2e11558cf30262a19a
rangaofei <876038559@qq.com>
2019-02-18 08:32:20 +0800

    Update sokit.rb

--- Formula/sokit.rb
+++ Formula/sokit.rb
@@ -5,6 +5,7 @@
   url "https://raw.githubusercontent.com/rangaofei/SWeather/master/pack/sokit-1.1.1.tar.gz"
   sha256 "78913c71cd3afdc2ab5d43ed5dbe51abf7d2c7e78959e210766c280b78e82cb8"
   version "1.1.1"
+  depends_on "qt"
   def install
 	  bin.install "sokit"
   end
