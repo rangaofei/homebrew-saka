4d1cc503e4f172a73a2752ae571ce01cf2b97361
rangaofei <876038559@qq.com>
2019-02-20 15:27:05 +0800

    Update ssokit.rb

--- Formula/ssokit.rb
+++ Formula/ssokit.rb
@@ -2,7 +2,7 @@
   desc "The origin author and page:\nhttps://github.com/sinpolib/sokit\nfamous new tool for debug with socket\n ----by saka"
   homepage "https://juejin.im/post/5a770f17f265da4e80778caa"
   url "https://raw.githubusercontent.com/rangaofei/SSokit/master/pack/SSokit-1.1.2.zip"
-  sha256 "78913c71cd3afdc2ab5d43ed5dbe51abf7d2c7e78959e210766c280b78e82cb8"
+  sha256 "30f1b599f193f96f0426bdb89abcb350943a48fea06b31d62c74b74e47908e33"
   version "1.1.2"
   depends_on "qt"
   def install
